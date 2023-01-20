bb98b4e84caaf8160fcb8f18d34b53b59d87aa18
Samuel Barbosa <contato@smkbarbosa.eti.br>
2023-01-20 10:23:21 -0300

    cria repositorio de payment

---
+++ src/modules/payment/repository/transaction.repository.ts
@@ -0,0 +1,28 @@
+import PaymentGateway from "../gateway/payment.gateway";
+import Transaction from "../domain/transaction";
+import TransactionModel from "./transaction.model";
+import Id from "../../@shared/domain/value-object/id.value-object";
+
+export default class TransactionRepository implements PaymentGateway {
+    async save(input: Transaction): Promise<Transaction> {
+        await TransactionModel.create({
+            id: input.id.id,
+            orderId: input.orderId,
+            amount: input.amount,
+            status: input.status,
+            createdAt: input.createdAt,
+            updatedAt: input.updatedAt
+        });
+
+        return new Transaction({
+            id: input.id,
+            orderId: input.orderId,
+            amount: input.amount,
+            status: input.status,
+            createdAt: input.createdAt,
+            updatedAt: input.updatedAt
+
+        })
+
+    }
+}
